f24b030b3c4038b873f2582754a227fc2d4f0136
kiel-ashram <kiel.ashram001@gmail.com>
2021-06-06 15:49:57 -0700

    second push

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -4,6 +4,35 @@
   
 //   getUserRepos();
 
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+// var getUserRepos = function() {
+//     fetch("https://api.github.com/users/octocat/repos");
+//   };
+
+// var response = fetch("https://api.github.com/users/octocat/repos");
+//   console.log(response);
+
+// fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//   console.log("inside", response);
+// });
+  
+// console.log("outside");
+
+// fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+//   response.json().then(function(data) {
+//     console.log(data);
+//   });
+// });
+
+var getUserRepos = function(user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+getUserRepos("kiel-ashram")
