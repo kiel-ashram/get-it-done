96c5c93619f82db7b2cd82c1cf89031925a7d311
kiel-ashram <kiel.ashram001@gmail.com>
2021-06-13 08:28:47 -0700

    first

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -23,16 +23,29 @@
 //   });
 // });
 
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+};
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
   // make a request to the url
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-      console.log(data);
+  fresponse.json().then(function(data) {
+    displayRepos(data, user);
     });
-  });
+};
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos);
+  console.log(searchTerm);
 };
 
 getUserRepos("kiel-ashram")
+
+userFormEl.addEventListener("submit", formSubmitHandler);
